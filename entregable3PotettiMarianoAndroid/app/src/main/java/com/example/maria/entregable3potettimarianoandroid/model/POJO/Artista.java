efeeb16b50693562c20fe3f0bf309a6b3e534e0d
Unknown <marianopotetti@hotmail.com>
2018-07-15 17:09:19 -0300

    agregue data base no se si anda bien, y clase artista

---
+++ entregable3PotettiMarianoAndroid/app/src/main/java/com/example/maria/entregable3potettimarianoandroid/model/POJO/Artista.java
@@ -0,0 +1,44 @@
+package com.example.maria.entregable3potettimarianoandroid.model.POJO;
+
+public class Artista {
+
+    private String Influenced_by;
+    private String artistId;
+    private String name;
+    private String nationality;
+
+    public Artista(){
+    }
+
+    public String getInfluenced_by() {
+        return Influenced_by;
+    }
+
+    public void setInfluenced_by(String influenced_by) {
+        Influenced_by = influenced_by;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(String artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+}
